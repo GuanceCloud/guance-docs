a1ce7371d6b46d87c39979e5430926e1deb74206
陆宏鸣 <lhm@guance.com>
2023-01-06 14:49:46 +0800

    stats.sh 脚本增加文档行数统计

--- stats.sh
+++ stats.sh
@@ -2,6 +2,7 @@
 
 files=$(find $PWD | xargs ls -ld  | awk '{print $9}' | grep '\.md')
 char_count=0
+line_count=0
 file_count=0
 
 for f in $files; do
@@ -11,9 +12,14 @@ for f in $files; do
     char_count=$(($char_count + $c))
     file_count=$(($file_count + 1))
 
+    line=$(cat $f | wc -l)
+    line_count=$(($line_count + $line))
+
+
     echo $f '\t字符数: ' $c
 done
 
 echo
 echo '文档总数: '$file_count
+echo '文档总行数: '$line_count
 echo '文档总共字符数: '$char_count
